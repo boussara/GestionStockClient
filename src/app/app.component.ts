d1fff66d00a01eb3456454eb338747142e5a8eb7
Mirage <you@example.cominventoory@gmail.com>
2020-04-19 18:56:25 +0000

    navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   showsidebar:boolean=false;
+
+  onshowsidebarchange(showsidebar){
+
+    this.showsidebar=showsidebar;
+
+  }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  @Input()
+  showsidebar:boolean;
+  @Output()
+  showsidebarchange:EventEmitter<boolean>=new EventEmitter<boolean>();
   constructor() { }
 
   ngOnInit() {
-  }
+
+    }
+    affichersidebar()
+    {
+      this.showsidebar =! this.showsidebar;
+      this.showsidebarchange.emit(this.showsidebar);
+    }
 
 }
